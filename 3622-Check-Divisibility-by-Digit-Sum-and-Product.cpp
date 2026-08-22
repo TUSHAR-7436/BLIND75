dd6bec55e8b7b82ce4c15236a5ec63d16968f749
Tushar <145340628+TUSHAR-7436@users.noreply.github.com>
2026-08-22 11:52:32 +0530

    [3622] [Time Beats: 100.00%] [Memory Beats: 37.42%] - LeetPush

---
+++ 3622-Check-Divisibility-by-Digit-Sum-and-Product.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+
+        int temp = n;
+        int sum = 0;
+        int product = 1;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            sum += digit;
+            product *= digit;
+
+            temp /= 10;
+        }
+
+        int e = sum + product;
+
+        return n % e == 0;
+    }
+};
