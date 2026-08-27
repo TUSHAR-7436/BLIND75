fa73dc2ee16dd9d8e789593e7e8913930f994c92
Tushar <145340628+TUSHAR-7436@users.noreply.github.com>
2026-08-27 23:19:58 +0530

    Update 136-Single-Number.cpp

--- 136-single-number/136-Single-Number.cpp
+++ 136-single-number/136-Single-Number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int a = 0;
+        int b = 0;
         for(int i=0;i<nums.size();i++){
-            a=a^nums[i];
+            b=b^nums[i];
         }
-        return a;
+        return b;
     }
 };
