6dbb6b57a8ff74a025a3af9a162795a63fc62822
Tushar <145340628+TUSHAR-7436@users.noreply.github.com>
2026-09-03 15:40:16 +0530

    [36] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 36-valid-sudoku/36-Valid-Sudoku.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> cols(9);
+        vector<unordered_set<char>> squares(9);
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.')
+                    continue;
+                char num = board[r][c];
+                int box = (r / 3) * 3 + (c / 3);
+                if (rows[r].count(num) ||
+                    cols[c].count(num) ||
+                    squares[box].count(num)) {
+                    return false;
+                }
+                rows[r].insert(num);
+                cols[c].insert(num);
+                squares[box].insert(num);
+            }
+        }
+        return true;
+    }
+};
